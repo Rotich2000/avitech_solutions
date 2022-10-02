ffe0c26d001e82da32896a370d5d0d83bba2df19
Rotich2000 <drkimutai06@gmail.com>
2022-10-02 21:39:21 +0300

    Changed some parts

--- src/components/WebsiteImage/WebsiteImage.js
+++ src/components/WebsiteImage/WebsiteImage.js
@@ -6,6 +6,7 @@ const WebsiteImage = () => {
   return (
     <div className="app__website-image">
       <h1>Our Packages</h1>
+      <h3>Installation fee: 2.500</h3>
       <div className="app__website-content">
         <img src={fiber} alt="product" />
       </div>
--- src/container/home/Home.js
+++ src/container/home/Home.js
@@ -1,19 +1,19 @@
-import React from 'react'
-import { Brand, Feature, Technician } from '../../components'
-import Important from '../../components/Important/Important'
-import {SliderData} from '../../components/feature/SliderData'
+import React from "react";
+import { Brand, Feature, Technician } from "../../components";
+import Important from "../../components/Important/Important";
+import { SliderData } from "../../components/feature/SliderData";
 
 const Home = () => {
   return (
     <>
-     <div className="gradient__bg">
-     <Technician/>
-     </div>
-     <Brand/>
-     <Feature slides={SliderData}/>
-     <Important/>
+      <div className="gradient__bg">
+        <Technician />
+      </div>
+      {/* <Brand/> */}
+      <Feature slides={SliderData} />
+      <Important />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
